28c7213c47c6c3eff41406f2290c432822de66ab
yanxujun <helloyxj@163.com>
2017-01-03 09:54:17 +0800

    3.登录界面的Model层定义 Model层定义的方法是什么? 所需的数据操作，login是P——>M，回调Callback是M——>P

---
+++ app/src/main/java/com/yxj/mvpsample/login/ILoginModel.java
@@ -0,0 +1,22 @@
+package com.yxj.mvpsample.login;
+
+import com.yxj.mvpsample.bean.ResultBean;
+
+/**
+ * Created by yxj on 17/1/3.
+ */
+
+public interface ILoginModel {
+
+    /*
+     * Model层定义的方法是什么?
+     * 所需的数据操作，login是P——>M，回调Callback是M——>P
+     */
+
+    interface Callback{
+        void onSucceed(ResultBean result);
+        void onFailed(ResultBean result);
+    }
+
+    void login(String account,String pwd,Callback callback);
+}
